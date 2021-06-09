7f39ff83b6f9d464268c1f17d99a8f54a5334db4
Aline Andrade <marson.aline@gmail.com>
2021-06-08 22:01:58 -0300

    Versão final de códigos básico e hacker edition testados

--- src/cipher.js
+++ src/cipher.js
@@ -4,8 +4,21 @@ const cipher = {
     let palavraCifrada = ""
     for (let i = 0; i < palavraDividida.length; i++) {
       let valDeslC = offset % 26
-      let numDeslocadoC = (letras.indexOf(palavraDividida[i]) + valDeslC) % 26
-      let numEmLetraC = letras.charAt(numDeslocadoC)
+      let numEmLetraC;
+      if (palavraDividida[i].charCodeAt(0) >= 65 && palavraDividida[i].charCodeAt(0) <= 91) {
+        let numDeslocadoC = (letras.indexOf(palavraDividida[i]) + valDeslC) % 26
+        numEmLetraC = letras.charAt(numDeslocadoC)
+      }
+      else if (palavraDividida[i].charCodeAt(0) >= 97 && palavraDividida[i].charCodeAt(0) <= 123) {
+        let numDeslocadoC = (letrasm.indexOf(palavraDividida[i]) + valDeslC) % 26
+        numEmLetraC = letrasm.charAt(numDeslocadoC)
+      }
+      else if (!isNaN(parseInt(palavraDividida[i]))) { //ele é um número, só que não? ou ele é um o contrário de um não número?//
+        throw new TypeError("Digite apenas letras e símbolos, não números")
+      }
+      else (
+        numEmLetraC = palavraDividida[i]
+      )
       palavraCifrada = palavraCifrada + numEmLetraC
     }
     return palavraCifrada
@@ -14,16 +27,35 @@ const cipher = {
     let palavraDividida = string.split("")
     let palavraDecifrada = ""
     for (let i = 0; i < palavraDividida.length; i++) {
-      let numDeslocadoD = (letras.indexOf(palavraDividida[i]) - offset)
-      if (numDeslocadoD < 0)
-        numDeslocadoD = 26 - (numDeslocadoD * -1) % 26
-      numDeslocadoD = numDeslocadoD % 26
-      let numEmLetraD = letras.charAt(numDeslocadoD)
+      let numEmLetraD
+      if (palavraDividida[i].charCodeAt(0) >= 65 && palavraDividida[i].charCodeAt(0) <= 91) {
+        let numDeslocadoD = (letras.indexOf(palavraDividida[i]) - offset)
+        if (numDeslocadoD < 0)
+          numDeslocadoD = 26 - (numDeslocadoD * -1) % 26
+        numDeslocadoD = numDeslocadoD % 26
+        numEmLetraD = letras.charAt(numDeslocadoD)
+      }
+      else if (palavraDividida[i].charCodeAt(0) >= 97 && palavraDividida[i].charCodeAt(0) <= 123) {
+        let numDeslocadoD = (letrasm.indexOf(palavraDividida[i]) - offset)
+        if (numDeslocadoD < 0)
+          numDeslocadoD = 26 - (numDeslocadoD * -1) % 26
+        numDeslocadoD = numDeslocadoD % 26
+        numEmLetraD = letrasm.charAt(numDeslocadoD)
+      }
+      else if (!isNaN(parseInt(palavraDividida[i]))) { //ele é um número, só que não? ou ele é um o contrário de um não número?//
+        throw new TypeError("Digite apenas letras e símbolos, não números")
+      }
+      else {
+        numEmLetraD = palavraDividida[i]
+      }
       palavraDecifrada = palavraDecifrada + numEmLetraD
-
     }
     return palavraDecifrada
   }
 };
 const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const letrasm = "abcdefghijklmnopqrstuvwxyz"
 export default cipher;
+
+
+
